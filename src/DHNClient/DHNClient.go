2e6dd26d7a9ae8ad5a79bfff03671cab896ccd9b
dohwe0528@gmail.com <dohwe0528@gmail.com>
2025-05-27 16:36:07 +0900

    added ssadago

--- src/DHNClient/DHNClient.go
+++ src/DHNClient/DHNClient.go
@@ -28,9 +28,14 @@ import (
 // 	description = "올지니 카카오 발송 프로그램"
 // )
 
+// const (
+// 	name        = "DHNClient_o"
+// 	description = "오투오 카카오 발송 프로그램"
+// )
+
 const (
-	name        = "DHNClient_o"
-	description = "오투오 카카오 발송 프로그램"
+	name        = "DHNClient_s"
+	description = "싸다고 카카오 발송 프로그램"
 )
 
 var dependencies = []string{name+".service"}
